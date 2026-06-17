8e08cb9f83f63aeffcfbfc10c01cfc94ae167527
silva <sangaremoussam719@gmail.com>
2026-06-17 18:16:10 +0100

    Premier commit

---
+++ javascript1.js
@@ -0,0 +1,5 @@
+// Exemple d’interaction JS
+document.querySelector("form").addEventListener("submit", function(e) {
+  e.preventDefault();
+  alert("Merci pour votre message ! Nous vous répondrons bientôt.");
+});
